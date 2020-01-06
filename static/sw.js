2f19dcd9829f54dec2fcbd46050d092341403991
Rengga Prakoso Nugroho <14950309+vzrenggamani@users.noreply.github.com>
2020-01-06 22:55:22 +0700

    Fixes workbox service worker

---
+++ static/sw.js
@@ -0,0 +1,49 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+
+if (workbox) {
+    console.log(`Yay! Workbox is loaded 🎉`);
+  
+    workbox.precaching.precacheAndRoute([]);
+
+    addEventListener('message', (event) => {
+      if (event.data && event.data.type === 'SKIP_WAITING') {
+        skipWaiting();
+      }
+    });
+
+    const articleHandler = workbox.strategies.networkFirst({
+      cacheName: 'articles-cache',
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 50,
+        })
+      ]
+    });
+    
+    workbox.routing.registerRoute(/(.*)article(.*)\.html/, args => {
+      return articleHandler.handle(args).then(response => {
+        if (!response) {
+          return caches.match('pages/offline.html');
+        } else if (response.status === 404) {
+          return caches.match('pages/404.html');
+        }
+        return response;
+      });
+    });
+
+    workbox.routing.registerRoute(
+      /(.*)articles(.*)\.(?:png|gif|jpg)/,
+      workbox.strategies.cacheFirst({
+        cacheName: 'images-cache',
+        plugins: [
+          new workbox.expiration.Plugin({
+            maxEntries: 50,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+          })
+        ]
+      })
+    );
+  
+  } else {
+    console.log(`Boo! Workbox didn't load 😬`);
+  }
--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,9 @@
 module.exports = {
   "globDirectory": "public/",
   "globPatterns": [
-    "**/*.{html,png,css,webmanifest}"
+    "**/*.{html,png,woff2,woff,js,css,xml,ico,webmanifest}"
   ],
-  "swDest": "public/service-worker-rengga.js"
+  "swDest": "public/service-worker-rengga.js",
+  "swSrc": "public/sw.js",
+  "cleanupOutdatedCaches": true
 };
